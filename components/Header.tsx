0ae3c8af40d43633a4b4c3335fb835ba3c66895e
mariam-adepoju <marrizon.am@gmail.com>
2026-01-11 17:34:42 +0100

    refactor: update header layout to match Figma

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,10 @@ import { Button } from "./ui/button"
 
 const Header = () => {
     return (
-        <header className="flex border-red-200 border items-center justify-between">
+        <header className="flex items-center justify-between">
             <div className="flex flex-col">
                 <p className="text-sm text-slate-400">POWERED BY</p>
-                <Image src="/safedep-logo.svg" alt="Logo" width={145} height={32} />
+                <Image src="https://mintcdn.com/safedep/SBr4leJ2Q_jWmTGH/images/full-logo-for-light-theme.png?fit=max&auto=format&n=SBr4leJ2Q_jWmTGH&q=85&s=c5133b0c57a88228719710fb4c94ff90" alt="Logo" width={145} height={32} priority />
             </div>
             <Button className="self-end px-4 py-2 flex items-center gap-2"><Image src="/github-logo.svg" alt="logo" width={13} height={13} /><span className="text-sm font-medium leading-5">Install GitHub App</span></Button>
         </header>
