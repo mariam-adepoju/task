15bd8598acd14b69c301088bb75b1f28086ac1b3
mariam-adepoju <marrizon.am@gmail.com>
2026-01-12 00:25:26 +0100

    refactor: align vulnerabilitytable component with Figma design

--- components/package/AnalysisTabs.tsx
+++ components/package/AnalysisTabs.tsx
@@ -29,7 +29,7 @@ export function AnalysisTabs({ data }: AnalysisTabsProps) {
     );
 
     return (
-        <Tabs defaultValue="overview" className="w-full">
+        <Tabs defaultValue="overview" className="w-full gap-0">
             <div className="border px-4 py-2 bg-background">
                 <TabsList className="bg-transparent h-14 gap-6">
                     {tabs.map((tab) => (
